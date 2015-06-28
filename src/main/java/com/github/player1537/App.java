784dc5a0c2c75efa3a740c44029abf951479bc04
Tanner Hobson <thobson125@gmail.com>
2015-06-28 00:51:30 -0400

    Added code to retrieve top posts from a subreddit

--- src/main/java/com/github/player1537/App.java
+++ src/main/java/com/github/player1537/App.java
@@ -1,10 +1,22 @@
 package com.github.player1537;
 
+import org.json.simple.parser.ParseException;
+
+import com.github.jreddit.entity.Submission;
+import com.github.jreddit.entity.User;
+import com.github.jreddit.exception.RedditError;
+import com.github.jreddit.exception.RetrievalFailedException;
+import com.github.jreddit.retrieval.Submissions;
+import com.github.jreddit.retrieval.params.SubmissionSort;
+import com.github.jreddit.utils.restclient.HttpRestClient;
+import com.github.jreddit.utils.restclient.PoliteHttpRestClient;
+
 import java.io.InputStream;
 import java.io.IOException;
 
 import java.lang.RuntimeException;
 
+import java.util.List;
 import java.util.Properties;
 
 public class App {
@@ -34,17 +46,11 @@ public class App {
 		assert properties.containsKey(key);
 	}
 
-	private void run() {
-		Properties properties = getProperties(CONFIG_FILENAME);
+	private String getUserAgent(Properties properties) {
 		assertExists(properties, "reddit.username");
-		assertExists(properties, "reddit.password");
 		assertExists(properties, "project.version");
 		assertExists(properties, "project.groupId");
 		assertExists(properties, "project.artifactId");
-
-		for (String key : properties.stringPropertyNames()) {
-			System.out.println("'" + key + "' = '" + properties.get(key) + "'");
-		}
 
 		String userAgentFormat = "java:%s.%s:v%s (by /u/%s)";
 		String userAgent = String.format(userAgentFormat,
@@ -53,5 +59,55 @@ public class App {
 		                                 properties.get("project.version"),
 		                                 properties.get("reddit.username"));
 		System.out.println("User agent: " + userAgent);
+
+		return userAgent;
+	}
+
+	private void run() {
+		Properties properties = getProperties(CONFIG_FILENAME);
+
+		for (String key : properties.stringPropertyNames()) {
+			System.out.println("'" + key + "' = '" + properties.get(key) + "'");
+		}
+
+		String userAgent = getUserAgent(properties);
+
+		HttpRestClient restClient = new PoliteHttpRestClient();
+		restClient.setUserAgent(userAgent);
+
+		assertExists(properties, "reddit.username");
+		assertExists(properties, "reddit.password");
+
+		User user = new User(restClient,
+		                     properties.getProperty("reddit.username"),
+		                     properties.getProperty("reddit.password"));
+
+		try {
+			user.connect();
+		} catch (IOException ex) {
+			throw new RuntimeException("Connecting user", ex);
+		} catch (ParseException ex) {
+			throw new RuntimeException("Connecting user", ex);
+		}
+
+		try {
+			Submissions subms = new Submissions(restClient, user);
+
+			List<Submission> submissions = subms.ofSubreddit("dogecoin",
+			                                                 SubmissionSort.TOP,
+			                                                 -1 /* count */,
+			                                                 100 /* limit */,
+			                                                 null /* before */,
+			                                                 null /* after */,
+			                                                 true /* show all */);
+
+			for (Submission submission : submissions) {
+				System.out.println(submission);
+			}
+		} catch (RetrievalFailedException ex) {
+			ex.printStackTrace();
+		} catch (RedditError ex) {
+			ex.printStackTrace();
+		}
 	}
 }
