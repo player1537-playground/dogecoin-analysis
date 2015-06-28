ff38d4f0bfe7582c9406768cce71e81417509a5c
Tanner Hobson <thobson125@gmail.com>
2015-06-27 22:10:32 -0400

    Added Maven autogenerated structure

---
+++ src/main/java/com/github/player1537/App.java
@@ -0,0 +1,13 @@
+package com.github.player1537;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
